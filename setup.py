507c343cc96b0eb79ce6779b72b4a87227d498ca
droefs <droefs@science.ru.nl>
2021-07-06 18:14:26 +0200

    Updated version number as the new API is incompatible with the old one

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hlisa',
-      version='0.5.0',
+      version='1.0.0',
       description='A reimplementation of the Selenium API, emulating human interactions',
       url='https://github.com/droefs/HLISA',
       maintainer='droefs',
