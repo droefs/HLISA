3ad692a4300b7d4320513877247144ccce6312b9
droefs <droefs@science.ru.nl>
2022-04-26 20:24:06 +0200

    Update test file to be independent of OpenWPM

--- test/test.py
+++ test/test.py
@@ -1,90 +1,90 @@
-class HLISATests(BaseCommand):
-    """This command runs HLISA tests"""
+#!/usr/bin/env python
 
-    def __init__(self) -> None:
-        self.logger = logging.getLogger("openwpm")
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium_firefox import FirefoxBinary, FirefoxLogInterceptor, Options
+from HLISA.hlisa_action_chains import HLISA_ActionChains
+import os.path
+import sys
+import time
 
-    def __repr__(self) -> str:
-        return "HLISATestsCommand"
+fb = FirefoxBinary(firefox_path="firefox/firefox")
+webdriver = webdriver.Firefox(firefox_binary=fb)
 
-    def execute(
-        self,
-        webdriver,
-        browser_params,
-        manager_params,
-        extension_socket,
-    ) -> None:
 
-        from HLISA.hlisa_action_chains import HLISA_ActionChains
+webdriver.maximize_window()
+webdriver.set_page_load_timeout(30)
 
-        webdriver.maximize_window()
-        webdriver.get('http://localhost:8000/')
 
-        # Clicking test:
-        actions = HLISA_ActionChains(webdriver)
+webdriver.get('http://localhost:8000/')
 
-        button1 = webdriver.find_element_by_id("button1")
-        for i in range(10):
-            actions.click(button1)
+# Clicking test:
+actions = HLISA_ActionChains(webdriver)
+actions.scroll_by(100, 100)
+button1 = webdriver.find_element_by_id("button1")
+for i in range(10):
+    actions.click(button1)
 
-        # Context clicking test:
+# Context clicking test:
 
-        button2 = webdriver.find_element_by_id("button2")
-        actions.context_click(button2)
+button2 = webdriver.find_element_by_id("button2")
+actions.context_click(button2)
 
-        # Test whether cursor location is correct after scroll:
+# Test whether cursor location is correct after scroll:
 
-        actions.scroll_by(0, 100)
-        button3 = webdriver.find_element_by_id("button3")
-        actions.click(button3)
+actions.scroll_by(0, 100)
+button3 = webdriver.find_element_by_id("button3")
+actions.click(button3)
         
-        actions.perform()
+actions.perform()
 
-        # Test whether the cursor location is correcter after a different page has been loaded:
+# Test whether the cursor location is correcter after a different page has been loaded:
 
-        webdriver.get('http://localhost:8000/page2.html')
+webdriver.get('http://localhost:8000/page2.html')
 
-        actions.reset_actions()
-        button1 = webdriver.find_element_by_id("button1")
-        actions.click(button1)
+actions.reset_actions()
+button1 = webdriver.find_element_by_id("button1")
+actions.click(button1)
 
-        # Test drag and drop:
+# Test drag and drop:
 
-        draggable_element = webdriver.find_element_by_id("draggable_element")
-        drag_endpoint = webdriver.find_element_by_id("drag_endpoint")
-        actions.drag_and_drop(draggable_element, drag_endpoint)
+draggable_element = webdriver.find_element_by_id("draggable_element")
+drag_endpoint = webdriver.find_element_by_id("drag_endpoint")
+actions.drag_and_drop(draggable_element, drag_endpoint)
 
-        # Element with offset test:
+# Element with offset test:
 
-        element_with_offset = webdriver.find_element_by_id("element_with_offset")
-        actions.move_to_element_with_offset(element_with_offset, 12, 34)
-        actions.click()
+element_with_offset = webdriver.find_element_by_id("element_with_offset")
+actions.move_to_element_with_offset(element_with_offset, 12, 34)
+actions.click()
 
-        # Double click test:
+# Double click test:
 
-        double_click_element = webdriver.find_element_by_id("double_click")
-        actions.double_click(double_click_element)
+double_click_element = webdriver.find_element_by_id("double_click")
+actions.double_click(double_click_element)
 
-        # Test drag and drop with offset:
+# Test drag and drop with offset:
 
-        draggable_element_offset = webdriver.find_element_by_id("draggable_element_offset")
-        drag_endpoint_offset = webdriver.find_element_by_id("drag_endpoint_offset")
-        actions.drag_and_drop_by_offset(draggable_element_offset, 500, 80)
+draggable_element_offset = webdriver.find_element_by_id("draggable_element_offset")
+drag_endpoint_offset = webdriver.find_element_by_id("drag_endpoint_offset")
+actions.drag_and_drop_by_offset(draggable_element_offset, 500, 80)
 
-        # Send keys to element test:
+# Send keys to element test:
 
-        send_keys_to_element_test = webdriver.find_element_by_id("send_keys_to_element_test")
-        actions.send_keys_to_element(send_keys_to_element_test, "test test")
+send_keys_to_element_test = webdriver.find_element_by_id("send_keys_to_element_test")
+actions.send_keys_to_element(send_keys_to_element_test, "test test")
 
-        # Key up and key down test:
+# Key up and key down test:
 
-        key_up_key_down_test = webdriver.find_element_by_id("key_up_key_down_test")
-        actions.key_down("a", key_up_key_down_test)
-        actions.key_up("a")
-        actions.key_down("b")
-        actions.key_up("b")
+key_up_key_down_test = webdriver.find_element_by_id("key_up_key_down_test")
+actions.key_down("a", key_up_key_down_test)
+actions.key_up("a")
+actions.key_down("b")
+actions.key_up("b")
 
-        # End of tests
+# End of tests
 
-        actions.pause(50)
-        actions.perform()
+actions.pause(50)
+actions.perform()
