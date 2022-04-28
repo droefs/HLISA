4667f607ea91ac118c7e13021b59de20f30560da
bkrumnow <benjamin.krumnow@th-koeln.de>
2022-04-28 12:04:54 +0200

    Add basic exceptions

---
+++ src/HLISA/errors.py
@@ -0,0 +1,15 @@
+class HLISAException(Exception):
+  """ Base class of all exception classes used by HLISA.
+  """
+  pass
+        
+class OutOfViewportException(HLISAException):
+  """ Indicates failing interaction due to leaving of the viewport
+  """
+  pass
+
+        
+
+          
+
+          
